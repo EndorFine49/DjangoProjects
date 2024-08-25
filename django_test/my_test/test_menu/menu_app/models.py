05d43c61d066237822e6b6e322e5a091d43843c3
EndorFine <anton.work49@gmail.com>
2024-08-25 22:00:29 +0300

    test_menu works

--- django_test/my_test/test_menu/menu_app/models.py
+++ django_test/my_test/test_menu/menu_app/models.py
@@ -1,17 +1,42 @@
 from django.db import models
 
-# Create your models here.
-class MenuItem(models.Model):
-    name = models.CharField('Название', max_length=100)
-    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
 
-    def children(self):
-        return self.__class__.objects.filter(parent=self.id)
-    
+class Menu(models.Model):
+    name = models.CharField(
+        'Название', max_length=100)
+    description = models.TextField(
+        'Описание', max_length=300, blank=True)
+
+    # class Meta:
+    #     ordering = ['id']
+    #     verbose_name = 'Меню'
+    #     verbose_name_plural = 'Меню'
+
     def __str__(self):
         return self.name
-    
-    class Meta:
-        ordering=('position,')
-        verbose_name='Пункт меню'
-        verbose_name_plural= 'Пункты меню'
+
+
+class MenuItem(models.Model):
+    name = models.CharField(
+        'Пункт меню', max_length=100)
+    # description = models.TextField(
+    #     'Описание', max_length=300, blank=True)
+    url = models.CharField(
+        verbose_name='URL-адрес стороннего ресурса',
+        help_text=(
+            'Указывается для перехода на ресурс из конечного пункта меню, '
+            'если не указать, то алгоритм будет пытаться найти потомков '
+            'данного пункта меню и создать из них подменю'),
+        max_length=50, blank=True)
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True)
+    menu = models.ForeignKey(
+        Menu, on_delete=models.CASCADE, related_name='menu_items')
+
+    # class Meta:
+    #     ordering = ['id']
+    #     verbose_name = 'Пункт меню'
+    #     verbose_name_plural = 'Пункты меню'
+
+    def __str__(self):
+        return self.name
